5e66871a63e04d0c42687a5886c692bb59679550
chen-luke <72319988+chen-luke@users.noreply.github.com>
2025-10-21 15:04:17 -0400

    added padding back

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -48,7 +48,7 @@ function Dashboard({ defaultHeader = true, courses, course, setCourse, addNewCou
   }
 
   return (
-    <div className="">
+    <div className="p-4">
       <h1>{`${defaultHeader ? "Dashboard" : "Pick A Course"}`}</h1><hr />
       <h5 ref={formRef}>Course</h5>
       <form onSubmit={addNewCourse}>
